780c8829f2c12142feae4ad1f5c9f641c45374ab
Mel Davis <melanie@nickel.local>
2020-09-16 08:23:22 -0400

    improve loading time

--- data/mergewizard/domain/DataCache.py
+++ data/mergewizard/domain/DataCache.py
@@ -6,71 +6,14 @@ from PyQt5.QtCore import pyqtSignal, QObject, qInfo
 from mobase import IOrganizer
 from mergewizard.domain.plugin import Plugins, PluginLoader
 from mergewizard.domain.merge import MergeFile as Merge, MergeFileReader
-from mergewizard.domain.AsyncWorker import Worker
+from mergewizard.domain.DataLoader import DataLoader
 from mergewizard.domain.MOLog import moPerf, moTime
 from mergewizard.models.PluginModel import PluginModel
 from mergewizard.models.MergeModel import MergeModel
 
 
-class ProgressEmitter(QObject):
-    def __init__(self, callback: Callable[[], int] = None, maxValue=0):
-        super().__init__()
-        self._callback = callback
-        self._maxValue = maxValue
-        self._pluginProgress = 0
-        self._mergeProgress = 0
-        self._dataCacheProgress = 0
-
-    def reset(self):
-        self._pluginProgress = 0
-        self._mergeProgress = 0
-        self._dataCacheProgress = 0
-        self._maxValue = 300
-
-    @property
-    def maxValue(self):
-        return self._maxValue
-
-    @maxValue.setter
-    def maxValue(self, value):
-        self._maxValue = value
-
-    def pluginProgress(self, amount: int) -> None:
-        # if not amount % 10:
-        #    qInfo("Plugin progress: {}".format(amount))
-        self._pluginProgress = amount
-        self._emitProgress()
-
-    def mergeProgress(self, amount: int) -> None:
-        # qInfo("Merge progress: {}".format(amount))
-        self._mergeProgress = amount
-        self._emitProgress()
-
-    def dataCacheProgress(self, amount: int) -> None:
-        # if not amount % 5:
-        #    qInfo("Data progress: {}".format(amount))
-        self._dataCacheProgress = amount
-        self._emitProgress()
-
-    def _emitProgress(self):
-        self._callback.emit(
-            (self._pluginProgress + self._mergeProgress + self._dataCacheProgress) * 100 / self._maxValue
-        )
-
-    def end(self) -> None:
-        self._value = self._maxValue
-        if self._callback:
-            self._callback.emit(100)
-
-
 class DataCache(QObject):
 
-    mergeModelLoadingStarted = pyqtSignal()
-    mergeModelLoadingProgress = pyqtSignal(int)
-    mergeModelLoadingCompleted = pyqtSignal()
-    pluginModelLoadingStarted = pyqtSignal()
-    pluginModelLoadingProgress = pyqtSignal(int)
-    pluginModelLoadingCompleted = pyqtSignal()
     dataCacheLoadingStarted = pyqtSignal()
     dataCacheLoadingProgress = pyqtSignal(int)
     dataCacheLoadingCompleted = pyqtSignal()
@@ -80,16 +23,9 @@ class DataCache(QObject):
         self._organizer: IOrganizer = organizer
         self._mergeModel: MergeModel = MergeModel()
         self._pluginModel: PluginModel = PluginModel(organizer)
-        self._pluginLoader: PluginLoader = None
-        self._mergeLoader: MergeFileReader = None
+        self._dataLoader: DataLoader = None
 
-        self._isLoadingData: bool = False
-
-        self._mergeStartTime: float = 0
-        self._pluginStartTime: float = 0
-        self._dataStartTime: float = 0
-
-        self._progressEmitter = ProgressEmitter(self.dataCacheLoadingProgress)
+        self._dataLoadingStartTime: float = 0
 
         # CACHED Data
         self.__merges = None
@@ -110,107 +46,45 @@ class DataCache(QObject):
         return self._organizer
 
     @property
-    def isLoadingData(self) -> bool:
-        return self.isLoadingMerges or self.isLoadingPlugins
-
-    @property
-    def isLoadingMerges(self) -> bool:
-        return self._mergeLoader and self._mergeLoader.isRunning()
-
-    @property
-    def isLoadingPlugins(self) -> bool:
-        return self._pluginLoader and self._pluginLoader.isRunning()
+    def isLoading(self) -> bool:
+        return self._dataLoader and self._dataLoader.isRunning()
 
     # --------------------------------------------------------
 
     def stopLoading(self) -> None:
-        if self.isLoadingMerges:
-            self._mergeLoader.stop()
-        if self.isLoadingPlugins:
-            self._pluginLoader.stop()
+        if self.isLoading:
+            self._dataLoader.stop()
 
     def loadData(self) -> None:
-        if self._isLoadingData:
+        if self.isLoading:
             return
-        self._isLoadingData = True
-        self._dataStartTime = perf_counter()
-        moTime(self._dataStartTime, "Started data loading")
-        self.dataCacheLoadingStarted.emit()
-        self._progressEmitter.reset()
-        self.loadMerges()
-        self.loadPlugins()
+        self._dataLoader = DataLoader(self._organizer)
+        self._dataLoader.finished.connect(self._finishedLoadingData)
+        self._dataLoader.result.connect(self._setData)
+        self._dataLoader.started.connect(self.dataCacheLoadingStarted)
+        self._dataLoader.finished.connect(self.dataCacheLoadingCompleted)
+        self._dataLoader.progress.connect(self.dataCacheLoadingProgress)
 
-    def loadMerges(self) -> None:
-        if self._mergeLoader and self._mergeLoader.isRunning():
-            return
-        self._mergeLoader = MergeFileReader(self._organizer.modsPath())
-        self._mergeLoader.started.connect(lambda: qInfo("Merge loader started"))
-        self._mergeLoader.progress.connect(self._progressEmitter.mergeProgress)
-        self._mergeLoader.finished.connect(self._finishedLoadingMerges)
-        self._mergeLoader.result.connect(self._setMerges)
-        self._mergeLoader.started.connect(self.mergeModelLoadingStarted)
-        self._mergeLoader.finished.connect(self.mergeModelLoadingCompleted)
-        self._mergeLoader.progress.connect(self.mergeModelLoadingProgress)
+        self._dataLoadingStartTime = perf_counter()
+        moTime(self._dataLoadingStartTime, "Data loading - started")
+        self._dataLoader.start()
 
-        self._mergeStartTime = perf_counter()
-        moTime(self._mergeStartTime, "Started loading merges")
-        self._mergeLoader.start()
-
-    def loadPlugins(self) -> None:
-        if self._pluginLoader and self._pluginLoader.isRunning():
-            return
-        self._pluginLoader = PluginLoader(self._organizer)
-        self._mergeLoader.started.connect(lambda: qInfo("Plugin loader started"))
-        self._pluginLoader.progress.connect(self._progressEmitter.pluginProgress)
-        self._pluginLoader.finished.connect(self._finishedLoadingPlugins)
-        self._pluginLoader.result.connect(self._setPlugins)
-        self._pluginLoader.started.connect(self.pluginModelLoadingStarted)
-        self._pluginLoader.finished.connect(self.pluginModelLoadingCompleted)
-        self._pluginLoader.progress.connect(self.pluginModelLoadingProgress)
-
-        self._pluginStartTime = perf_counter()
-        moTime(self._pluginStartTime, "Started loading plugins")
-        self._pluginLoader.start()
-
-    def _finishedLoadingMerges(self) -> None:
-        self._mergeLoader.disconnect()
-        self._mergeLoader = None
-        moPerf(self._mergeStartTime, perf_counter(), "Finished loading merges")
-
-    def _finishedLoadingPlugins(self) -> None:
-        self._pluginLoader.disconnect()
-        self._pluginLoader = None
-        moPerf(self._pluginStartTime, perf_counter(), "Finished loading plugins")
+    def _finishedLoadingData(self) -> None:
+        self._dataLoader.disconnect()
+        self._dataLoader = None
+        moPerf(self._dataLoadingStartTime, perf_counter(), "Data loading - complete")
 
     # ------------------------------------------------
     # Combine the info from the merge model into the
     # plugin model
     # ------------------------------------------------
 
-    def _setMerges(self, merges: List[Merge]):
-        self.__merges = merges
-        self.mergeModel.setMerges(merges)
-        self._combineModels()
+    def _setData(self, data):
+        startTime = perf_counter()
+        moTime(startTime, "Loading data into models - started")
+        self.__plugins = data[0]
+        self.pluginModel.setPlugins(deepcopy(self.__plugins))
+        self.__merges = data[1]
+        self.mergeModel.setMerges(self.__merges)
+        moPerf(startTime, perf_counter(), "Loading data into models - complete")
 
-    def _setPlugins(self, plugins: Plugins):
-        self.__plugins = plugins
-        self.pluginModel.setPlugins(deepcopy(plugins))
-        self._combineModels()
-
-    def _combineModels(self):
-        if self.isLoadingMerges:
-            return
-        if self.isLoadingPlugins:
-            return
-
-        count = 0
-        total = len(self.__merges)
-        for merge in self.__merges:
-            self.pluginModel.addPlugin(merge.filename, modName=merge.modName, isMerge=True)
-            for pluginFileDesc in merge.plugins:
-                self.pluginModel.addPlugin(pluginFileDesc.filename, modName=pluginFileDesc.modName, isMerged=True)
-            count = count + 1
-            self._progressEmitter.dataCacheProgress(count * 100 / total)
-        self._progressEmitter.end()
-        self.dataCacheLoadingCompleted.emit()
-        moPerf(self._dataStartTime, perf_counter(), "Finished loading data")
---
+++ data/mergewizard/domain/DataLoader.py
@@ -0,0 +1,113 @@
+from typing import Set
+from os import path
+from glob import glob
+from PyQt5.QtCore import QThread, pyqtSignal
+from mobase import IOrganizer, PluginState
+from mergewizard.domain.plugin.Plugin import Plugin
+from mergewizard.domain.plugin.Plugins import Plugins
+from mergewizard.domain.merge.MergeFile import MergeFile
+from mergewizard.domain.MOLog import moWarn
+
+
+class DataLoader(QThread):
+    """ Loads all plugins that MO2 knows about. """
+
+    progress = pyqtSignal(int)
+    result = pyqtSignal(object)
+
+    def __init__(self, organizer: IOrganizer):
+        super().__init__()
+        self.__organizer = organizer
+        self._stopped = False
+        self._plugins = None
+        self._merges = None
+        self._pluginNames = None
+        self._mergeFiles = None
+
+    def stop(self):
+        self._stopped = True
+
+    def run(self):
+        # first get enough info to know now how often we
+        # should emit progress
+        self._pluginNames = self.__organizer.pluginList().pluginNames()
+        self._mergeFiles = glob(self.__organizer.modsPath() + "/*/*/merge.json")
+
+        self._total = len(self._pluginNames) * 2 + len(self._mergeFiles)
+        self._count = 0
+        self._progress = 0
+
+        self.loadPlugins()
+        self.loadMergeFiles()
+        self.result.emit((self._plugins, self._merges))
+
+    def loadPlugins(self):
+        plugins = Plugins()
+        for name in self._pluginNames:
+            if self._stopped:
+                return
+            self.emitProgress()
+            plugins.add(self.loadPlugin(name))
+        for name in self._pluginNames:
+            self.emitProgress()
+            for requirement in self.__organizer.pluginList().masters(name):
+                if self._stopped:
+                    return
+                plugins.addRequirement(plugins.get(name), plugins.get(requirement, False))
+        self._plugins = plugins
+
+    def loadPlugin(self, name: str) -> Plugin:
+        plugin = Plugin(name)
+        plugin.priority = self.__organizer.pluginList().priority(name)
+        plugin.isMaster = self.__organizer.pluginList().isMaster(name)
+        state = self.__organizer.pluginList().state(name)
+        plugin.isInactive = state != PluginState.ACTIVE
+        plugin.isMissing = state == PluginState.MISSING
+        mod = self.__organizer.getMod(self.__organizer.pluginList().origin(name))
+        if mod:
+            plugin.modName = mod.name()
+            plugin.modPath = mod.absolutePath()
+        return plugin
+
+    def loadMergeFiles(self):
+        merges: Set[MergeFile] = set()
+
+        for file in self._mergeFiles:
+            if self._stopped:
+                return
+            self.emitProgress()
+            try:
+                merge = self.loadMergeFromFile(file)
+                merge.mergePath = file
+                merge.modName = path.basename(path.dirname(path.dirname(file)))
+                merges.add(merge)
+                self.addMergeToPlugins(merge)
+            except OSError as ex:
+                moWarn('Failed to open merge file "{}": {}'.format(file, ex.strerror))
+            except ValueError as ex:
+                moWarn('Failed to read merge file "{}": {}'.format(file, ex))
+            # Now we add the merge info to the plugins
+        self._merges = merges
+
+    def loadMergeFromFile(self, filepath) -> MergeFile:
+        with open(filepath, "r", encoding="utf8") as f:
+            merge = MergeFile.fromJSON(f.read())
+            return merge
+
+    def addMergeToPlugins(self, mergeFile):
+        plugin = self._plugins.get(mergeFile.filename, False)
+        plugin.isMerge = True
+        if not plugin.modName:
+            plugin.modName = mergeFile.modName
+        for pfd in mergeFile.plugins:
+            plugin = self._plugins.get(pfd.filename, False)
+            plugin.isMerged = True
+            if not plugin.modName:
+                plugin.modName = mergeFile.modName
+
+    def emitProgress(self):
+        self._count = self._count + 1
+        v = int(self._count * 100 / self._total)
+        if v != self._progress:
+            self._progress = v
+            self.progress.emit(v)
