a8a8714752fbbdc0c007bbd6953f5c6aa5228aab
Mel Davis <melanie@nickel.local>
2020-09-21 13:59:50 -0400

    add validation to settings dialog's zedit path

--- data/mergewizard/dialogs/SettingsDialog.py
+++ data/mergewizard/dialogs/SettingsDialog.py
@@ -1,4 +1,5 @@
-from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QDir
+from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtWidgets import QWidget, QDialog, QFileDialog
 from mergewizard.domain.Context import Context
 from mergewizard.constants import Setting, Icon
@@ -10,8 +11,10 @@ class SettingsDialog(QDialog):
         super().__init__(parent)
         self.ui = Ui_SettingsDialog()
         self.ui.setupUi(self)
+        self.ui.errorIndicator.setPixmap(QPixmap(Icon.ERROR))
         self.ui.zeditPathButton.setIcon(QIcon(Icon.FOLDER))
         self.ui.zeditPathButton.clicked.connect(lambda: self.showFileDialog())
+        self.ui.zeditPathEdit.textChanged.connect(lambda: self.validateZEditPath())
 
     def loadSettings(self, context: Context):
         # set ui widget values
@@ -27,6 +30,7 @@ class SettingsDialog(QDialog):
         self.ui.zeditPathEdit.setText(context.zEditFolder)
         self.ui.modNameTemplate.setText(context.modNameTemplate)
         self.ui.profileNameTemplate.setText(context.profileNameTemplate)
+        self.validateZEditPath()
 
     def storeSettings(self, context: Context):
         context.storeUserSetting(Setting.ENABLE_HIDING_PLUGINS, self.ui.enableHiding.isChecked())
@@ -47,4 +51,12 @@ class SettingsDialog(QDialog):
         )
         if dirName:
             self.ui.zeditPathEdit.setText(dirName)
+        self.validateZEditPath()
+
+    def validateZEditPath(self):
+        dir = QDir(self.ui.zeditPathEdit.text())
+        if dir.exists("zedit.exe"):
+            self.ui.errorIndicator.setVisible(False)
+        else:
+            self.ui.errorIndicator.setVisible(True)
 
