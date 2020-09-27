67932bf5d99a0a3551c50932822ba0b61ca4720c
Mel Davis <melanie@nickel.local>
2020-09-27 08:14:38 -0400

    do not allow editing zedit path on page

--- data/mergewizard/dialogs/PageZMerge.py
+++ data/mergewizard/dialogs/PageZMerge.py
@@ -1,6 +1,6 @@
 from PyQt5.QtCore import QDir, qInfo, QVariant
 from PyQt5.QtGui import QIcon, QPixmap
-from PyQt5.QtWidgets import QWidget, QFileDialog, QComboBox
+from PyQt5.QtWidgets import QWidget, QComboBox
 from mergewizard.dialogs.WizardPage import WizardPage
 from mergewizard.domain.Context import Context
 from mergewizard.domain.merge.ZEditConfig import ZEditConfig
@@ -15,33 +15,31 @@ class PageZMerge(WizardPage):
         self.ui.setupUi(self)
         self.context = context
         self.profiles = []
+        self.zEditFolder = ""
         self.lastProfile = context.getSetting("lastZMergeProfile", QVariant.String, "")
 
-        self.ui.folderError.setPixmap(QPixmap(Icon.ERROR))
+        # error indicators
+        self.ui.warningIcon.setPixmap(QPixmap(Icon.ERROR))
+        self.ui.warningFrame.setVisible(False)
         self.ui.profileError.setPixmap(QPixmap(Icon.ERROR))
         self.ui.pluginNameError.setPixmap(QPixmap(Icon.ERROR))
         self.ui.modNameError.setPixmap(QPixmap(Icon.ERROR))
 
-        self.setZEditFolderError()
         self.setZMergeProfileError()
         self.setPluginNameError()
         self.setModNameError()
 
         self.ui.modName.setEditable(True)
         self.ui.modName.setInsertPolicy(QComboBox.InsertAtTop)
-        self.ui.zeditPathButton.setIcon(QIcon(Icon.FOLDER))
-        self.ui.zeditPathButton.clicked.connect(lambda: self.showFileDialog())
-        self.ui.zeditPathEdit.textChanged.connect(lambda: self.validateZEditPath())
         self.ui.pluginName.textChanged.connect(lambda: self.validatePluginName())
         self.ui.zmergeProfile.currentIndexChanged.connect(self.loadMergeNames)
         self.ui.modName.currentIndexChanged.connect(self.loadMergeFile)
         self.context.settingChanged.connect(self.settingChanged)
 
     def initializePage(self):
+        self.initializeZEditPath()
         self.loadProfiles()
         self.initializeMOProfileName()
-        self.initializeZEditPath()
-        self.initializeZMergeProfile()
         self.initializeNames()
         self.validatePanel()
 
@@ -49,14 +47,81 @@ class PageZMerge(WizardPage):
         self.context.setSetting("lastZMergeProfile", self.ui.zmergeProfile.currentText())
         return True
 
+    def initializeZEditPath(self):
+        self.zEditFolder = self.context.zEditFolder
+        if not self.zEditFolder:
+            self.ui.warningFrame.setVisible(True)
+        else:
+            dir = QDir(self.context.zEditFolder)
+            if dir.exists("zedit.exe"):
+                self.ui.warningFrame.setVisible(False)
+            else:
+                self.ui.warningFrame.setVisible(True)
+                self.zEditFolder = ""
+
+    def initializeMOProfileName(self):
+        # TODO: If we allow creating merges to other MO profile, change this.
+        # For now this is okay.
+        self.ui.moProfile.setText(self.context.profile.currentProfileName())
+
+    def loadProfiles(self):
+        """ This is called whenever the Wizard moves forward to this page and whenever the
+        zEdit folder changes in Settings """
+
+        self.profiles = ZEditConfig.getProfiles(self.context.profile.gameName(), self.zEditFolder)
+        self.ui.zmergeProfile.clear()
+        for i in range(len(self.profiles)):
+            name = self.profiles[i].profileName
+            self.ui.zmergeProfile.addItem(name, i)
+            if name == self.lastProfile:
+                self.ui.zmergeProfile.setCurrentIndex(i)
+
+    def loadMergeNames(self, profileRow: int):
+        """ Fills in the modName combobox with the mod names from the selected profile.
+        This is called by the change signal from the profile combob box """
+
+        self.ui.modName.clear()
+        if self.profiles[profileRow].merges:
+            self.ui.modName.insertSeparator(0)
+
+        selectedMerge = self.context.mergeModel.selectedMergeName()
+        for i in range(len(self.profiles[profileRow].merges)):
+            mergeName = self.profiles[profileRow].merges[i].name
+            self.ui.modName.addItem(mergeName, i)
+            if mergeName == selectedMerge:
+                self.ui.modName.setCurrentIndex(self.ui.modName.count() - 1)
+
+    def loadMergeFile(self, modNameRow: int):
+        """ Sets the name of the plugin (it it can) and loads the merge view.
+        This is called by the change signal from the modName combo box """
+
+        currentMerge = self.ui.modName.currentData()
+        if currentMerge is None:
+            self.calculateNewPluginName()
+            return
+        currentProfile = self.ui.zmergeProfile.currentData()
+        if currentProfile is not None:
+            self.ui.pluginName.setText(self.profiles[currentProfile].merges[currentMerge].filename)
+
+    def calculateNewPluginName(self):
+        self.ui.pluginName.clear()
+
+    def initializeNames(self):
+        selectedMerge = self.context.dataCache.mergeModel.selectedMergeName()
+        # self.ui.modName.setText(selectedMerge)
+
+    def settingChanged(self, setting: int):
+        if setting == Setting.ZEDIT_FOLDER:
+            self.zEditFolder = self.context.zEditFolder
+            self.initializeZMergeProfile()
+        if setting == Setting.PROFILENAME_TEMPLATE:
+            self.initializeNames()
+        if setting == Setting.MODNAME_TEMPLATE:
+            self.initializeNames()
+
     # ----
     # ---- Validation and Error handling
     # ----
-
-    def setZEditFolderError(self, msg=None):
-        if msg:
-            self.ui.folderError.setToolTip(msg)
-        self.ui.folderError.setVisible(bool(msg))
 
     def setZMergeProfileError(self, msg=None):
         if msg:
@@ -73,34 +138,16 @@ class PageZMerge(WizardPage):
             self.ui.modNameError.setToolTip(msg)
         self.ui.modNameError.setVisible(bool(msg))
 
-    def showFileDialog(self):
-        dirName = QFileDialog.getExistingDirectory(
-            self, self.tr("zEdit Directory"), self.ui.zeditPathEdit.text(), QFileDialog.ShowDirsOnly
-        )
-        if dirName:
-            self.ui.zeditPathEdit.setText(dirName)
-        self.validateZEditPath()
-
     def validatePanel(self):
-        self.validateZEditPath()
         self.validateZMergeProfile()
         self.validatePluginName()
         self.validateModName()
-
-    def validateZEditPath(self):
-        if not self.ui.zeditPathEdit.text():
-            self.setZEditFolderError("Path cannot be empty.")
-        dir = QDir(self.ui.zeditPathEdit.text())
-        if dir.exists("zedit.exe"):
-            self.setZEditFolderError()
-        else:
-            self.setZEditFolderError(self.tr('Path does not include "zedit.exe".'))
 
     def validateZMergeProfile(self):
         if self.ui.zmergeProfile.currentIndex() < 0:
             self.setZMergeProfileError("Unable to find a zMerge profile for this game.")
         else:
-            self.setZEditFolderError()
+            self.setZMergeProfileError()
 
     def validatePluginName(self):
         if not self.ui.pluginName.text():
@@ -111,64 +158,3 @@ class PageZMerge(WizardPage):
     def validateModName(self):
         pass
 
-    # ----
-    # ---- Initializing and setting values
-    # ----
-
-    def settingChanged(self, setting: int):
-        if setting == Setting.ZEDIT_FOLDER:
-            self.ui.zeditPathEdit.setText("")
-            self.initializeZMergeProfile()
-        if setting == Setting.PROFILENAME_TEMPLATE:
-            self.initializeNames()
-        if setting == Setting.MODNAME_TEMPLATE:
-            self.initializeNames()
-
-    def initializeMOProfileName(self):
-        # TODO: If we allow creating merges to other MO profile, change this.
-        # For now this is okay.
-        self.ui.moProfile.setText(self.context.profile.currentProfileName())
-
-    def initializeZEditPath(self):
-        if not self.ui.zeditPathEdit.text():
-            self.ui.zeditPathEdit.setText(self.context.zEditFolder)
-
-    def initializeZMergeProfile(self):
-        pass
-
-    def initializeNames(self):
-        selectedMerge = self.context.dataCache.mergeModel.selectedMergeName()
-        # self.ui.modName.setText(selectedMerge)
-
-    def loadProfiles(self):
-        self.profiles = ZEditConfig.getProfiles(self.context.profile.gameName(), self.context.zEditFolder)
-        self.ui.zmergeProfile.clear()
-        for i in range(len(self.profiles)):
-            name = self.profiles[i].profileName
-            self.ui.zmergeProfile.addItem(name, i)
-            if name == self.lastProfile:
-                self.ui.zmergeProfile.setCurrentIndex(i)
-
-    def loadMergeNames(self, profileRow: int):
-        self.ui.modName.clear()
-        if self.profiles[profileRow].merges:
-            self.ui.modName.insertSeparator(0)
-
-        selectedMerge = self.context.mergeModel.selectedMergeName()
-        for i in range(len(self.profiles[profileRow].merges)):
-            mergeName = self.profiles[profileRow].merges[i].name
-            self.ui.modName.addItem(mergeName, i)
-            if mergeName == selectedMerge:
-                self.ui.modName.setCurrentIndex(self.ui.modName.count() - 1)
-
-    def loadMergeFile(self, modNameRow: int):
-        currentMerge = self.ui.modName.currentData()
-        if currentMerge is None:
-            self.calculateNewPluginName()
-            return
-        currentProfile = self.ui.zmergeProfile.currentData()
-        if currentProfile is not None:
-            self.ui.pluginName.setText(self.profiles[currentProfile].merges[currentMerge].filename)
-
-    def calculateNewPluginName(self):
-        self.ui.pluginName.clear()
